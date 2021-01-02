76a8c6f2a028a731334981dffa71fa281e97a470
Edmilson Vasconcelos <edmilson.vasconcelos@compasso.com.br>
2021-01-01 21:05:55 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 import './config/connection';
 
@@ -10,7 +11,16 @@ class App {
         this.routes();
     }
     middlewares() {
-        this.app.use(express.json())
+        this.app.use(express.json());
+        this.app.use(cors());
+
+        this.app.use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE');
+            res.header("Access-Control-Allow-Headers", 'X-PINGOTHER, Content-Type');
+            this.app.use(cors());
+            next();
+        })
     }
     routes() {
         this.app.use(routes);
